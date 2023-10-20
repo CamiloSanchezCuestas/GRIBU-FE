47f4c741c96045f7405004bd29dbb2e69b12f2a7
SantiagoNeira21 <santigla047@gmail.com>
2023-10-19 21:39:24 -0500

    Implementacion calificacion estrellas y texto en vimeoPlayer

--- src/components/VimeoPlayer.jsx
+++ src/components/VimeoPlayer.jsx
@@ -1,19 +1,13 @@
-import React from 'react';
-import { View, StyleSheet, TouchableOpacity, Image } from 'react-native';
-import { Vimeo } from 'react-native-vimeo-iframe';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { Vimeo } from 'react-native-vimeo-iframe';
+import StarRating from 'react-native-star-rating';
 import CustomBottomBar from './NavegationBar';
-
-const videoCallbacks = {
-  timeupdate: (data) => console.log('timeupdate: ', data),
-  play: (data) => console.log('play: ', data),
-  pause: (data) => console.log('pause: ', data),
-  fullscreenchange: (data) => console.log('fullscreenchange: ', data),
-  ended: (data) => console.log('ended: ', data),
-};
 
 const VimeoPlayer = () => {
   const navigation = useNavigation();
+  const [rating, setRating] = useState(0);
 
   const goToProfile = () => {
     navigation.navigate('Profile');
@@ -23,13 +17,49 @@ const VimeoPlayer = () => {
     navigation.navigate('HomeScreen');
   };
 
+  const onStarRatingPress = (rating) => {
+    setRating(rating);
+    console.log(`Has seleccionado ${rating} estrellas`);
+    
+  };
+
+  const videoCallbacks = {
+    timeupdate: (data) => console.log('timeupdate: ', data),
+    play: (data) => console.log('play: ', data),
+    pause: (data) => console.log('pause: ', data),
+    fullscreenchange: (data) => console.log('fullscreenchange: ', data),
+    ended: (data) => console.log('ended: ', data),
+  };
+
   return (
     <View style={styles.container}>
       <TouchableOpacity style={styles.buttonImage} onPress={() => navigation.navigate('HomeScreen')}>
         <Image source={require('../../assets/icons/return.png')} style={styles.imageButton} />
       </TouchableOpacity>
 
-      <Vimeo videoId={'835587844'} params={'api=1&autoplay=1'} handlers={videoCallbacks} style={styles.vimeoPlayer} />
+      <Vimeo
+        style={styles.vimeoPlayer}
+        videoId={'861756713'}
+        params={'api=1&autoplay=1'}
+        handlers={videoCallbacks}
+      />
+
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>
+          Pasos para escuchar con atención y generar una conexión
+        </Text>
+      </View>
+
+      <View style={styles.starRatingContainer}>
+        <StarRating
+          disabled={false}
+          maxStars={5}
+          rating={rating}
+          selectedStar={(rating) => onStarRatingPress(rating)}
+          starSize={25}
+          fullStarColor="gold"
+        />
+      </View>
 
       <CustomBottomBar
         style={styles.navigation}
@@ -54,13 +84,28 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    paddingBottom: 220,
+    paddingBottom: 100,
     backgroundColor: '#FFFF',
   },
   vimeoPlayer: {
-    marginHorizontal: 10, // Aplicar un margen horizontal de 10
+    margin: 10,
+    top:60
   },
-  navigation: {},
+  starRatingContainer: {
+    alignItems: 'flex-start',
+    paddingLeft: 15,
+    paddingBottom:250
+   
+  },
+  textContainer: {
+    backgroundColor: '#f5f4f4',
+    margin: 10,
+    alignItems: 'center',
+  },
+  text: {
+    color: 'black',
+    fontSize: 16,
+  },
 });
 
 export default VimeoPlayer;
