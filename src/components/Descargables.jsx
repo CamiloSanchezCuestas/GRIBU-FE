50c0d4cfee0811b4acadf9697dcf52e78cb445a2
SantiagoNeira21 <santigla047@gmail.com>
2023-10-26 17:51:36 -0500

    Descargables

--- src/components/Descargables.jsx
+++ src/components/Descargables.jsx
@@ -4,7 +4,6 @@ import { useNavigation } from '@react-navigation/native';
 import CustomBottomBar from './NavegationBar';
 import * as FileSystem from 'expo-file-system';
 
-
 const Descargables = () => {
   const navigation = useNavigation();
 
@@ -14,6 +13,31 @@ const Descargables = () => {
 
   const goToHomeScreen = () => {
     navigation.navigate('HomeScreen');
+  };
+
+  const downloadFile = async () => {
+    const fileUri = 'https://sheetjs.com/pres. Numbers';
+    const fileUriComponents = fileUri.split('/');
+    const fileName = fileUriComponents[fileUriComponents.length - 1];
+    const destinationUri = `${FileSystem.documentDirectory}${fileName}`;
+
+    const callback = (downloadProgress) => {
+      const progress = downloadProgress.totalBytesWritten / downloadProgress.totalBytesExpectedToWrite;
+      console.log(progress);
+    };
+
+    const downloadResumable = FileSystem.createDownloadResumable(fileUri, destinationUri, {}, callback);
+
+    try {
+      const result = await downloadResumable.downloadAsync();
+      if (result.status === 200) {
+        console.log('File downloaded successfully:', result.uri);
+      } else {
+        console.error('Failed to download file. HTTP status:', result.status);
+      }
+    } catch (error) {
+      console.error('Error while downloading:', error);
+    }
   };
 
   return (
@@ -26,7 +50,7 @@ const Descargables = () => {
       </View>
       <View style={styles.content}>
         <TouchableOpacity style={styles.card}>
-          <TouchableOpacity style={styles.cardTextContainer} onPress={() => navigation.navigate('VimeoPlayer')}>
+          <TouchableOpacity style={styles.cardTextContainer} onPress={downloadFile}>
             <Image source={require('../../assets/images/CMUAA.png')} style={styles.imagepdf1} />
           </TouchableOpacity>
         </TouchableOpacity>
@@ -40,58 +64,52 @@ const Descargables = () => {
     </View>
   );
 };
-  
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      
-    },
-    buttonImage: {
-        position: 'absolute',
-        top: 50,
-        left: 10,
-        zIndex: 1,
-      },
-      imageButton: {
-        width: 60,
-        height: 60,
-      },
-      
-    imageH:{
-        width: 380,
-        height: 80,
-      },
-      imagepdf1:{
-        width: 380,
-        height: 220,
-        marginLeft:82,
-        marginTop:-30
-        
-      },
-      imagepdf2:{
-        width: 383,
-        height: 228,
-        marginLeft:82,
-        marginTop:-40
-      },
-    header: {
-      flexDirection: 'row',
-      justifyContent: 'center',
-      marginBottom: 30,
-      marginTop:120
-    },
-   
 
-    card: {
-      width:250,
-      height:250,     
-      padding: 16,
-      marginLeft:30,
-      borderRadius: 8,
-      alignItems:'center'
-    },
- 
-  });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  buttonImage: {
+    position: 'absolute',
+    top: 50,
+    left: 10,
+    zIndex: 1,
+  },
+  imageButton: {
+    width: 60,
+    height: 60,
+  },
+  imageH: {
+    width: 380,
+    height: 80,
+  },
+  imagepdf1: {
+    width: 380,
+    height: 220,
+    marginLeft: 82,
+    marginTop: -30,
+  },
+  imagepdf2: {
+    width: 383,
+    height: 228,
+    marginLeft: 82,
+    marginTop: -40,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginBottom: 30,
+    marginTop: 120,
+  },
+  card: {
+    width: 250,
+    height: 250,
+    padding: 16,
+    marginLeft: 30,
+    borderRadius: 8,
+    alignItems: 'center',
+  },
+});
 
 export default Descargables;
