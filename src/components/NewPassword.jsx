37fa242e84582755013d1a5159e0756cdfe31bde
SantiagoNeira21 <santigla047@gmail.com>
2023-10-27 00:40:40 -0500

    Implementacion pantalla Perfil

--- src/components/NewPassword.jsx
+++ src/components/NewPassword.jsx
@@ -1,73 +1,112 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, ImageBackground } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, ImageBackground, Modal } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import { useNavigation } from '@react-navigation/native';
 
-export default function NewPassword({ navigation }) {
- 
-    const [newPassword, setNewPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [message, setMessage] = useState('');
-    const [isButtonDisabled, setButtonDisabled] = useState(false);
+export default function NewPassword() {
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [message, setMessage] = useState('');
+  const [isButtonDisabled, setButtonDisabled] = useState(false);
+  const [showInputBox, setShowInputBox] = useState(false);
+  const [MessageColor, setMessageColor] = useState("");
+  const navigation = useNavigation(); 
 
-    const handlePasswordChange = () => {
-        if (newPassword === confirmPassword) {
-            setMessage('La contraseña se cambió exitosamente');
-           
-            setButtonDisabled(true);
+  const handlePasswordChange = () => {
+    if (newPassword === confirmPassword) {
+        setMessage('');
+      setButtonDisabled(true);
 
-            setTimeout(() => {
-                navigation.navigate('InicioSesion');
-            }, 3000);
-        } else {
-            setMessage('Las contraseñas no coinciden');
-            
-        }
-    };
+      setTimeout(() => {
+        setShowInputBox(true); 
 
-    const [messageColor, setMessageColor] = useState(''); 
+        
+        setTimeout(() => {
+          setShowInputBox(false);
+          navigation.navigate('InicioSesion'); 
+        }, 3000);
+      }, 3000);
+    } else {
+      setMessage('Las contraseñas no coinciden');
+      setMessageColor('red');
+    }
+  };
 
-    return (
-        <LinearGradient
-        colors={['#60cbe3', '#ffff', '#ffff']} 
-        style={styles.container}
-        >
-        <View style={styles.overlay}>
-            <ImageBackground source={require('../../assets/images/PortadaRegistro.png')} style={styles.Image}></ImageBackground>
-        </View>
-       
-        <View style={styles.overlay}>
-            <TextInput
-              style={styles.input}
-              placeholder="Contraseña"
-              value={newPassword}
-              onChangeText={setNewPassword}
-            />
+  return (
+    <LinearGradient colors={['#60cbe3', '#ffff', '#ffff']} style={styles.container}>
+      <View style={styles.overlay}>
+        <ImageBackground source={require('../../assets/images/PortadaRegistro.png')} style={styles.Image} />
+      </View>
 
-            <TextInput
-              style={styles.input}
-              placeholder="Confirmar contraseña"
-              value={confirmPassword}
-              onChangeText={setConfirmPassword}
-            />
-                
-            <TouchableOpacity style={styles.SendButton} onPress={handlePasswordChange}>
-                <LinearGradient
-                    colors={['#480899','#8730f7']}
-                    start={{ x: 1, y: 0 }}
-                    end={{ x: 0, y: 0 }}
-                    style={{ ...styles.buttonGradient, borderRadius: 10 }}
-                >   
-                    <Text style={styles.buttonText}>Enviar</Text>
-                </LinearGradient>
-            </TouchableOpacity>
-            
-            <Text style={{ ...styles.errorText, color: "black" }}>{message}</Text>
-        </View>
+      <View style={styles.overlay}>
+        <TextInput
+          style={styles.input}
+          placeholder="Contraseña"
+          value={newPassword}
+          onChangeText={setNewPassword}
+        />
+
+        <TextInput
+          style={styles.input}
+          placeholder="Confirmar contraseña"
+          value={confirmPassword}
+          onChangeText={setConfirmPassword}
+        />
+
+        <TouchableOpacity style={styles.SendButton} onPress={handlePasswordChange}>
+          <LinearGradient
+            colors={['#480899', '#8730f7']}
+            start={{ x: 1, y: 0 }}
+            end={{ x: 0, y: 0 }}
+            style={{ ...styles.buttonGradient, borderRadius: 10 }}
+          >
+            <Text style={styles.buttonText}>Enviar</Text>
+          </LinearGradient>
+        </TouchableOpacity>
+
+        {showInputBox && (
+          <Modal transparent={true} animationType="slide">
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <Text style={styles.modalText}>TU CONTRASEÑA HA SIDO ACTUALIZADA!</Text>
+              </View>
+            </View>
+          </Modal>
+        )}
+
+        <Text style={{ ...styles.errorText, color: 'red' }}>{message}</Text>
+      </View>
     </LinearGradient>
-    );
+  );
 }
 
+
+
 const styles = StyleSheet.create({
+  
+
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    borderWidth:3,
+    borderColor:"#1878ec"
+  },
+  modalText: {
+    fontSize: 25,
+    marginBottom: 30,
+    textAlign: "center",
+    fontWeight:'900',
+    color:"#004aad"
+  },
+  
     container: {
         flex: 1,
         alignItems: 'center',
@@ -84,8 +123,6 @@ const styles = StyleSheet.create({
     input: {
         width: '90%',
         height: 40,
-        backgroundColor: 'rgba(255, 255, 255, 1)',
-        borderColor: 'gray',
         marginBottom: 20,
         paddingLeft: 10,
         borderRadius: 13,
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,22 +1,135 @@
 import React from "react";
-import { ScrollView, Text, View, Image, StyleSheet } from "react-native";
+import {
+    ScrollView,
+    Text,
+    View,
+    Image,
+    StyleSheet,
+    TouchableOpacity,
+    TextInput,
+  } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useNavigation } from '@react-navigation/native';
+import CustomBottomBar from './NavegationBar';
 
 const Profile = () => {
+    const goToLook = () => {
+      navigation.navigate("Buscador");
+    };
+    const goToProfile = () => {
+      navigation.navigate("Profile");
+    };
+    const goToLogin = () => {
+      navigation.navigate("InicioSesion");
+    };
+  
+    const navigation = useNavigation();
+  
     return (
-        <SafeAreaView>
-            <Text>
-                Esta es la página de su perfil
-            </Text>
-        </SafeAreaView>
+      <SafeAreaView style={styles.container}>
+        <TouchableOpacity
+          style={styles.buttonImage}
+          onPress={() => navigation.navigate("HomeScreen")}
+        >
+          <Image source={require("../../assets/icons/return.png")} style={styles.imageButton} />
+        </TouchableOpacity>
+  
+        <Image source={require("../../assets/icons/Profile.png")} style={styles.avatarImage} />
+        <Text style={styles.changeAvatarText}>CAMBIAR AVATAR</Text>
+        <View style={styles.textcontainer}>
+        <Text style={styles.labelText}>Nombre</Text>
+        </View>
+       
+
+        <View style={styles.inputContainer}>
+          <TextInput style={styles.blueText} value={"Nicolas Montes"} />
+        </View>
+
+        <View style={styles.textcontainer}>
+        <Text style={styles.labelText}>Usuario</Text>
+        </View>
+        <View style={styles.inputContainer}>
+          <TextInput style={styles.blueText} value={"Nicomontes9"} />
+        </View>
+  
+        <View style={styles.textcontainer}>
+        <Text style={styles.labelText}>      Contraseña</Text>
+        </View>
+        <View style={styles.inputContainer}>
+          <TextInput style={styles.blueText} value={"Contraseña"} secureTextEntry={true} />
+        </View>
+  
+        <View style={styles.textcontainer}>
+        <Text style={styles.labelText}>Correo</Text>
+        </View>
+        <View style={styles.inputContainer}>
+          <TextInput style={styles.blueText} value={"nicomomej@gmail.com"} />
+        </View>
+  
+        <CustomBottomBar goToProfile={goToProfile} goToHome={goToLogin} goToStats={goToProfile} />
+      </SafeAreaView>
     );
-}
-
-const styles = StyleSheet.create({
+  };
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+    },
     title: {
-        fontSize: 20,
-        marginTop: 50
-    }
-});
-
-export default Profile
+      fontSize: 20,
+      marginTop: 50,
+    },
+    buttonImage: {
+      position: "absolute",
+      top: 50,
+      left: 10,
+      zIndex: 1,
+    },
+    imageButton: {
+      width: 60,
+      height: 60,
+    },
+    textcontainer:{
+        
+    paddingRight:250,
+    
+    },
+    avatarImage: {
+      width: 130, 
+      height: 130, 
+      marginTop: 110,
+      marginBottom:10
+    },
+    changeAvatarText: {
+    
+      fontSize: 22,
+      color: "#27d8ff",
+      fontWeight:'900',
+      marginBottom:25
+    },
+    inputContainer: {
+      margin: 10,
+      borderWidth:2,
+      borderColor:'#aeb2b9',
+      backgroundColor:'#eff5fe',
+      width:320,
+      borderRadius:10,
+    },
+    labelText: {
+      color: "#979797",
+      fontSize: 15,
+      fontWeight:'800',
+    
+      
+    },
+    blueText: {
+      color: "#1e5fb7",
+      fontSize: 15,
+      fontWeight:'900',
+      margin:8 
+    },
+  });
+  
+  export default Profile;
